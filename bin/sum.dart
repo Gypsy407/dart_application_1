b4ecf4987c9fa741a7d10f4f8d2fb7b5788ad44a
Gypsy407 <vishnupv407@gmail.com>
2023-09-19 21:47:53 +0530

    commit

---
+++ bin/sum.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print('Enter  two numbers');
+  var a = stdin.readLineSync();
+  var b = stdin.readLineSync();
+
+  var n1 = int.parse(a!);
+  var n2 = int.parse(b!);
+
+  print("SUM of $a+$b = ${n1 + n2}");
+}
